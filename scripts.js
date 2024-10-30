71081bb80fece5fdfc0bae5885fda9d141bd837c
WilliamBarbosaWB <williambarbosa.videira@gmail.com>
2024-10-30 17:12:55 -0400

    avancei na montagem do projeto.

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+
+
+
+function inserirNumero(num) {
+    const telaCalculadora = document.querySelector(".display").value
+
+    document.querySelector(".display").value = telaCalculadora + num
+
+    
+}
